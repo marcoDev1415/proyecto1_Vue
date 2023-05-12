c98ef2c492fadf42de61dc568229ee3768ce3148
odues1415 <marcosoftwaredeveloper@gmail.com>
2023-05-11 23:15:40 -0600

    primer commit

---
+++ src/components/Counter.vue
@@ -0,0 +1,47 @@
+<template>
+   <h1>Counter</h1>
+   <p>{{ counter }} <sup>2</sup> = {{ counter * counter }} </p>
+
+   <p>{{ counter }} <sup>2</sup> = {{squareCounter}} </p>
+   <p>{{ counter }} <sup>2</sup> = {{squareCounter}} </p>
+
+   <p>{{ counter }} <sup>2</sup> = {{squareCounter}} </p>
+
+   <p>{{ counter }} <sup>2</sup> = {{squareCounter}} </p>
+
+   <p>{{ counter }} <sup>2</sup> = {{squareCounter}} </p>
+
+   <p>{{ counter }} <sup>2</sup> = {{getValue()}} </p>
+   
+   <p>{{ counter }} <sup>2</sup> = {{getValue()}} </p>
+  
+   
+   <p>{{ counter }} <sup>2</sup> = {{getValue()}} </p>
+  
+  
+</template>
+<script>
+  export default{
+    //name: 'counter
+    data(){
+      return{
+        counter: 5
+
+      }
+    },
+    methods:{
+      getValue(){
+        console.log('getValue');
+        return this.counter * this.counter;
+      }
+    },
+    computed:{
+      squareCounter(){
+        console.log('squareCounter')
+        return this.counter * this.counter
+      }
+    }
+  }
+</script>
+<style>
+</style>
